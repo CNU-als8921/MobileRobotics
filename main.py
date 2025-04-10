f63199ceb9f5b3d53a924ae78869ff8d3c20b81d
als8921 <als8921@naver.com>
2025-04-10 23:16:50 +0900

    fix : 코드 오류 수정

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class Robot:
 
     def drawRobot(self, line_length = 1, color = 'r'):
 
-        dx = line_length * np.cos(np.deg2rad(robot.theta))
-        dy = line_length * np.sin(np.deg2rad(robot.theta))
+        dx = line_length * np.cos(np.deg2rad(self.theta))
+        dy = line_length * np.sin(np.deg2rad(self.theta))
 
         plt.plot(self.x, self.y, 'o', markersize = 30, color=color, alpha = 0.3, zorder=5)
         plt.arrow(self.x, self.y, dx, dy, head_width=0, head_length=0, fc=color, ec=color,linewidth=1.5, zorder=5)
